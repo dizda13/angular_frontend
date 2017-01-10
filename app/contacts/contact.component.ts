222a4a52433b2e8396950e4b9f9cd29ccdc677e9
dizda13 <dino_dizda@live.com>
2017-01-10 14:07:44 +0100

    add ip address to contacts

--- app/contacts/contact.component.ts
+++ app/contacts/contact.component.ts
@@ -4,11 +4,13 @@ export class Contact{
   first_name: string;
   last_name: string;
   profile_picture: string;
+  ip_address: string;
   constructor(){
     this.username="";
     this.email="";
     this.first_name="";
     this.last_name="";
     this.profile_picture="";
+    this.ip_address="";
   }
 }
